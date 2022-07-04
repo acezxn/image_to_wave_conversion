ae9d5692186d426ce528cb960b6e594505f01206
acezxn <acezxn@gmail.com>
2022-07-04 08:50:48 +0800

    Update wav2image.py

--- wav2image.py
+++ wav2image.py
@@ -2,18 +2,16 @@ from scipy.io.wavfile import read
 from scipy.fft import fft, fftfreq
 import matplotlib.pyplot as plt
 import image2wav
+import sys
 import numpy as np
 from PIL import Image
 
 
 def map(arr, MIN, MAX):
-    minimum = min(arr)
-    maximum = max(arr)
-    print("b", minimum, maximum)
+    minimum = min(arr.tolist())
+    maximum = max(arr.tolist())
     out = []
     for i in arr:
-        # if ((i - minimum) / (maximum - minimum) * (MAX - MIN) + MIN > 150):
-        #     print((i - minimum) / (maximum - minimum) * (MAX - MIN) + MIN)
         out.append((i - minimum) / (maximum - minimum) * (MAX - MIN) + MIN)
     
     return out
@@ -33,47 +31,34 @@ def One2TwoD(arr, dim):
     print()
 
     map = image2wav.Two2OneD(TwoDMap)
-    print(map)
     for i, value in enumerate(arr):
-        print(i)
         key = map[i]
         out[key // dim][key % dim] = value
-
-    # for y in range(dim):
-    #     for x in range(dim):
-    #         print(f"{out[y][x]} ", end = "")
-    #     print()
-    # print()
 
     return out
 
 
 
 
-rate, data = read("output.wav")
+rate, data = read(sys.argv[1])
 print("Sample rate: {} Hz".format(rate))
 print("Data type: {}".format(data.dtype))
-
-# plt.figure(figsize=(15, 5))
-# plt.plot(data[0:22050])
-# plt.show()
 
 sample_rate = 44100
 duration = 0.5
 N = int(sample_rate * duration)
 
 dataFFT = fft(data[0:22050])
-x = fftfreq(N, 1 / sample_rate)[0:6314]
+x = fftfreq(N, 1 / sample_rate)[0:2093]
 
-dataFFTAbs = abs(dataFFT[0:6314])
-print(len(x), len(dataFFTAbs))
+dataFFTAbs = abs(dataFFT[0:2093])
 plt.figure(figsize=(15, 5))
 plt.plot(x, dataFFTAbs, 'r')
 plt.show()
 
-dimensions = 8
+dimensions = int(sys.argv[2])
 freq_resolution = dimensions ** 2
-step = 6314 / (freq_resolution-1)
+step = 2093 / (freq_resolution-1)
 
 freqs = []
 current = 0
@@ -83,7 +68,7 @@ prevGoal = 0;
 
 dataFFTAbs = map(dataFFTAbs, 0, 255)
 
-while current < 6314 and len(freqs) < freq_resolution:
+while current < 2093 and len(freqs) < freq_resolution:
     total += dataFFTAbs[int(current)] ** 40
     count += 1
     goal = int(prevGoal + step)
@@ -97,7 +82,6 @@ while current < 6314 and len(freqs) < freq_resolution:
     
     current += 1
         
-print(max(freqs))
 out = One2TwoD(freqs, dimensions)
 array = np.array(out, np.uint8)
 print(array)
